f8fc75a28bc0ded54e15fa2ae095c7e2c11bea9a
ThePaavero <pekka@astudios.org>
2020-09-23 21:27:56 +0300

    WIP

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-import '../stylesheets/style.scss';
+import '../stylesheets/style.scss'
 
 const $ = (selector) => {
   return document.querySelector(selector)
@@ -7,19 +7,48 @@ const $ = (selector) => {
 (() => {
   let canvas
   let context
-  const state = {}
+  const state = {
+    particles: [],
+  }
 
-  const randomIntFromInterval = (min, max) => {
+  const randomBetween = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
+  const createParticle = () => {
+    state.particles.push({
+      size: randomBetween(1, 10),
+      x: randomBetween(0, canvas.height),
+      y: randomBetween(0, canvas.width),
+      color: 'red',
+    })
+  }
+
+  const render = () => {
+    state.particles.forEach(particle => {
+      context.beginPath()
+      context.arc(particle.x, particle.y, particle.size, 0, 2 * Math.PI, false)
+      context.fillStyle = particle.color
+      context.fill()
+    })
+  }
+
   const tick = () => {
+    if (randomBetween(0, 100) === 0) {
+      createParticle()
+    }
+
+    render()
+
     window.requestAnimationFrame(tick)
   }
 
   const init = () => {
     canvas = $('canvas')
     context = canvas.getContext('2d')
+
+    canvas.width = 1440
+    canvas.height = 900
 
     tick()
   }
