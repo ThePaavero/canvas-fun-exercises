7379fd73f3951f5d8911ebb65a71ee451646df73
ThePaavero <pekka@astudios.org>
2020-09-23 21:48:39 +0300

    Dunno :)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,8 +18,8 @@ const $ = (selector) => {
   const createParticle = () => {
     state.particles.push({
       size: randomBetween(10, 100),
-      x: randomBetween(0, canvas.height),
-      y: randomBetween(0, canvas.width),
+      x: randomBetween(0, canvas.width),
+      y: randomBetween(0, canvas.height),
       color: getRandomColor(),
     })
   }
@@ -34,6 +34,7 @@ const $ = (selector) => {
   }
 
   const render = () => {
+    context.clearRect(0, 0, canvas.width, canvas.height)
     state.particles.forEach(particle => {
       context.beginPath()
       context.arc(particle.x, particle.y, particle.size, 0, 2 * Math.PI, false)
@@ -43,13 +44,18 @@ const $ = (selector) => {
   }
 
   const tick = () => {
-    if (randomBetween(0, 2) === 0) {
+    if (randomBetween(0, 1) === 0) {
       createParticle()
     }
 
-    if (state.particles.length > 100) {
+    if (state.particles.length > 600) {
       state.particles.splice(randomBetween(state.particles.length - 1), 1)[0]
     }
+
+    state.particles.forEach(particle => {
+      particle.x += randomBetween(-5, 5)
+      particle.y += randomBetween(-5, 5)
+    })
 
     render()
 
