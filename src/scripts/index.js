0b3565c39836c5dd9192eb18ae7f2eebb5e05a06
ThePaavero <pekka@astudios.org>
2020-12-15 23:59:14 +0200

    Started.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,8 +3,12 @@ import '../stylesheets/style.scss'
 (() => {
   let canvas
   let context
+
+  let maxBuildings = 0
+  let floor = 0
+
   const state = {
-    particles: [],
+    buildings: [],
   }
 
   const $ = (selector) => {
@@ -15,74 +19,38 @@ import '../stylesheets/style.scss'
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
-  const createParticle = () => {
-    state.particles.push({
-      size: randomBetween(30, 100),
-      x: randomBetween(0, canvas.width),
-      y: randomBetween(0, canvas.height),
-      color: getRandomColor(),
-    })
-  }
-
-  const getRandomColor = () => {
-    // var letters = '0123456789ABCDEF'
-    const letters = '56789ABCDEF'
-    let color = '#'
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)]
-    }
-    return color
-  }
-
-  const drawEyes = (particle) => {
-    const left = particle.x + (particle.size / 6)
-    const eyes = {
-      top: particle.y + (particle.size / 6),
-      left,
-      right: left + (particle.size / 2),
-      size: particle.size / 50,
-    }
-
-    const randomOffset = randomBetween(particle.size / 10, particle.size / 10 + (particle.size / 10))
-    eyes.left += randomBetween
-    eyes.right += randomBetween
-
-    context.beginPath()
-    context.arc(eyes.left, eyes.top, eyes.size, 0, 2 * Math.PI, false)
-    context.fillStyle = '#fff'
-    context.fill()
-
-    context.beginPath()
-    context.arc(eyes.right, eyes.top, eyes.size, 0, 2 * Math.PI, false)
-    context.fillStyle = '#fff'
-    context.fill()
-  }
-
   const render = () => {
     context.clearRect(0, 0, canvas.width, canvas.height)
-    state.particles.forEach(particle => {
-      context.beginPath()
-      context.arc(particle.x, particle.y, particle.size, 0, 2 * Math.PI, false)
-      context.fillStyle = particle.color
-      context.fill()
+    drawBuildings()
+  }
 
-      // drawEyes(particle)
+  const drawBuildings = () => {
+    state.buildings.forEach((building) => {
+      context.fillStyle = building.color;
+      context.fillRect(building.x, building.y, building.width, building.height)
     })
+  }
+
+  const createRandomBuilding = () => {
+    const height = randomBetween(10, 150)
+    const width = randomBetween(20, 40)
+    const y = floor - height
+    const building = {
+      x: randomBetween(-10, canvas.width),
+      y,
+      width,
+      height,
+      color: 'black',
+    }
+    console.log(building)
+    state.buildings.push(building)
   }
 
   const tick = () => {
-    if (randomBetween(0, 1) === 0) {
-      createParticle()
-    }
 
-    if (state.particles.length > 1000) {
-      state.particles.splice(randomBetween(state.particles.length - 1), 1)[0]
+    if (randomBetween(0, 10) === 0 && state.buildings.length < maxBuildings) {
+      createRandomBuilding()
     }
-
-    state.particles.forEach(particle => {
-      particle.x += randomBetween(-5, 5)
-      particle.y += randomBetween(-5, 5)
-    })
 
     render()
 
@@ -92,14 +60,10 @@ import '../stylesheets/style.scss'
   const init = () => {
     canvas = $('canvas')
     context = canvas.getContext('2d')
-
     canvas.width = 1920
     canvas.height = 1080
-
-    for (let i = 0; i < 1000; i++) {
-      createParticle()
-    }
-
+    maxBuildings = 20
+    floor = canvas.height - 140
     tick()
   }
 
