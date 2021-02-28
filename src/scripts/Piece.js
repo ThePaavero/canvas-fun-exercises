9cdce9fa51e7ac1ebc6560f465b3eed67d591c14
Pekka Siiriäinen <pekka.siiriainen@wundermanthompson.com>
2021-03-01 01:54:26 +0200

    WIP

--- src/scripts/Piece.js
+++ src/scripts/Piece.js
@@ -2,16 +2,28 @@ import { randomBetween } from './utils'
 
 const Piece = (canvas, context) => {
 
+  const planeWidth = 50
   const state = {
-    // ...
+    plane: {
+      x: (canvas.width / 2) + planeWidth / 2,
+      y: canvas.height / 7,
+      width: planeWidth,
+      height: 18,
+    },
+    targets: []
   }
 
   const stateTick = () => {
     // ...
   }
 
+  const drawPlane = () => {
+    context.fillStyle = '#000'
+    context.fillRect(state.plane.x, state.plane.y, state.plane.width, state.plane.height)
+  }
+
   const draw = () => {
-    // ...
+    drawPlane()
   }
 
   return {
