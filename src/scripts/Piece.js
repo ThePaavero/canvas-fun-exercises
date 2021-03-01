63a1ebe1fcac72c8f728269d1a020596371ab3f4
Pekka Siiriäinen <pekka.siiriainen@wundermanthompson.com>
2021-03-01 02:03:56 +0200

    WIP

--- src/scripts/Piece.js
+++ src/scripts/Piece.js
@@ -2,8 +2,10 @@ import { randomBetween } from './utils'
 
 const Piece = (canvas, context) => {
 
+  const floorHeight = 40
   const planeWidth = 50
   const state = {
+    gameSpeed: 1,
     plane: {
       x: (canvas.width / 2) + planeWidth / 2,
       y: canvas.height / 7,
@@ -13,8 +15,29 @@ const Piece = (canvas, context) => {
     targets: []
   }
 
+  const getFloor = () => {
+    return canvas.height - floorHeight
+  }
+
+  const createTargets = () => {
+    for (let i = 0; i < randomBetween(4, 8); i++) {
+      state.targets.push({
+        x: randomBetween(200, 400),
+        y: getFloor(),
+        width: randomBetween(200, 700),
+        height: randomBetween(20, 80)
+      })
+    }
+  }
+
+  const moveTargets = () => {
+    state.targets.forEach(target => {
+      target.x += state.gameSpeed
+    })
+  }
+
   const stateTick = () => {
-    // ...
+    moveTargets()
   }
 
   const drawPlane = () => {
@@ -22,9 +45,29 @@ const Piece = (canvas, context) => {
     context.fillRect(state.plane.x, state.plane.y, state.plane.width, state.plane.height)
   }
 
+  const drawFloor = () => {
+    context.fillStyle = '#000'
+    context.fillRect(0, canvas.height - floorHeight, canvas.width, floorHeight)
+  }
+
+  const drawTargets = () => {
+    state.targets.forEach(target => {
+      context.fillStyle = '#000'
+      context.fillRect(target.x, target.y, target.width, target.height)
+    })
+  }
+
   const draw = () => {
     drawPlane()
+    drawTargets()
+    drawFloor()
   }
+
+  const init = () => {
+    createTargets()
+  }
+
+  init()
 
   return {
     stateTick,
